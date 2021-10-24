6d93341982196b64f440cc1a40c3da979bf4e52b
Almir Menezes da Cunha Junior <almirmdacunha@gmail.com>
2021-10-24 16:17:29 -0300

    feat: generate population

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import random
+
 class EightQueens:
     _population = []
 
     def __init__(self) -> None:
         pass
+
+    def _fenotype_to_chromosome(self, fenotype):
+        chromosome = ''
+
+        for f in fenotype:
+            chromosome += format(f, '03b')
+
+        return chromosome
+    
+    def generate_population(self, size):
+        fenotype = [0, 1, 2, 3, 4, 5, 6, 7]
+        self._population = []
+        
+        for i in range(0, size):
+            random.shuffle(fenotype)
+            chromosome = self._fenotype_to_chromosome(fenotype)
+            self._population.append(chromosome)
+        
 def main():
     eigth_queens = EightQueens()
+
+    eigth_queens.generate_population(100)
 if __name__ == '__main__':
     main()
